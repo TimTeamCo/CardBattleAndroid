281d271e66290ac1bb007fb5c41aaa04014e473e
Ilya <ponomarenko.illia@kfccte-nau.ukr.education>
2022-11-03 19:45:08 +0100

    Debbuged squad attack script

--- Assets/GameEditor/Scripts/Battle/Squad/SquadAttack.cs
+++ Assets/GameEditor/Scripts/Battle/Squad/SquadAttack.cs
@@ -13,28 +13,24 @@ namespace TTBattle
             {
                 if (deffender._unit is Warrior)
                 {
-                    deffender.Count = (int) Math.Floor(
-                        (deffender._unit.Health * _playerDeffender._unitsInfluence[0] * deffender.Count -
-                         attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
-                        deffender._unit.Health * _playerDeffender._unitsInfluence[0]);
+                    deffender.Count = (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[0] * deffender.Count -
+                                              attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
+                                             (deffender._unit.Health * _playerDeffender._unitsInfluence[0]));
                     Debug.Log($"{deffender.Count} {attacker.Count}");
-                    attacker.Count = (int) Math.Floor(
-                        (attacker._unit.Health * _playerAttacker._unitsInfluence[0] * attacker.Count -
-                         deffender._unit.Attack * _playerDeffender._unitsInfluence[0] * deffender.Count) /
-                        attacker._unit.Health * _playerAttacker._unitsInfluence[0]);
+                    attacker.Count = (int) ((attacker._unit.Health * _playerAttacker._unitsInfluence[0] * attacker.Count -
+                                             deffender._unit.Attack * _playerDeffender._unitsInfluence[0] * deffender.Count) /
+                                            (attacker._unit.Health * _playerAttacker._unitsInfluence[0]));
                 }
 
                 if (deffender._unit is Assasin)
-                    deffender.Count = (int) Math.Floor(
-                        (deffender._unit.Health * _playerDeffender._unitsInfluence[1] * deffender.Count -
-                         attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
-                        deffender._unit.Health * _playerDeffender._unitsInfluence[1]);
+                    deffender.Count = (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[1] * deffender.Count -
+                                              attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
+                                             (deffender._unit.Health * _playerDeffender._unitsInfluence[1]));
                 if (deffender._unit is Mage)
                 {
-                    deffender.Count = (int) Math.Floor(
-                        (deffender._unit.Health * _playerDeffender._unitsInfluence[2] * deffender.Count -
-                         attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
-                        deffender._unit.Health * _playerDeffender._unitsInfluence[2]);
+                    deffender.Count = (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[2] * deffender.Count -
+                                              attacker._unit.Attack * _playerAttacker._unitsInfluence[0] * attacker.Count) /
+                                             (deffender._unit.Health * _playerDeffender._unitsInfluence[2]));
                     StartCoroutine(MageAttack(attacker, deffender, attacker.Count, _playerDeffender._unitsInfluence[2],
                         _playerDeffender));
                 }
@@ -44,25 +40,22 @@ namespace TTBattle
             {
                 if (deffender._unit is Warrior)
                     deffender.Count =
-                        (int) Math.Floor(
-                            (deffender._unit.Health * _playerDeffender._unitsInfluence[0] * deffender.Count -
-                             attacker._unit.Attack * _playerAttacker._unitsInfluence[1] * attacker.Count *
-                             attacker._unit.WeakCoeficient) / deffender._unit.Health *
-                            _playerDeffender._unitsInfluence[0]);
+                        (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[0] * deffender.Count -
+                                attacker._unit.Attack * _playerAttacker._unitsInfluence[1] * attacker.Count *
+                                attacker._unit.WeakCoeficient) / (deffender._unit.Health *
+                                                                  _playerDeffender._unitsInfluence[0]));
 
                 if (deffender._unit is Assasin)
-                    deffender.Count = (int) Math.Floor(
-                        (deffender._unit.Health * _playerDeffender._unitsInfluence[1] * deffender.Count -
-                         attacker._unit.Attack * _playerAttacker._unitsInfluence[1] * attacker.Count) /
-                        deffender._unit.Health * _playerDeffender._unitsInfluence[1]);
+                    deffender.Count = (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[1] * deffender.Count -
+                                              attacker._unit.Attack * _playerAttacker._unitsInfluence[1] * attacker.Count) /
+                                             (deffender._unit.Health * _playerDeffender._unitsInfluence[1]));
 
                 if (deffender._unit is Mage)
                     deffender.Count =
-                        (int) Math.Floor(
-                            (deffender._unit.Health * _playerDeffender._unitsInfluence[2] * deffender.Count -
-                             attacker._unit.Attack * _playerAttacker._unitsInfluence[2] * attacker.Count *
-                             attacker._unit.StrongCoefitient) / deffender._unit.Health *
-                            _playerDeffender._unitsInfluence[2]);
+                        (int) ((deffender._unit.Health * _playerDeffender._unitsInfluence[2] * deffender.Count -
+                                attacker._unit.Attack * _playerAttacker._unitsInfluence[2] * attacker.Count *
+                                attacker._unit.StrongCoefitient) / (deffender._unit.Health *
+                                                                    _playerDeffender._unitsInfluence[2]));
             }
 
             if (attacker._unit is Mage)
@@ -78,29 +71,26 @@ namespace TTBattle
                 if (_deffender._unit is Warrior)
                 {
                     yield return _deffender.Count =
-                        (int) Math.Floor(
-                            (_deffender._unit.Health * _deffenderPlayer._unitsInfluence[0] * _deffender.Count -
-                             _attacker._unit.Attack * _mageInfluence * _attackCount *
-                             _attacker._unit.StrongCoefitient) / _deffender._unit.Health *
-                            _deffenderPlayer._unitsInfluence[0]);
+                        (int) ((_deffender._unit.Health * _deffenderPlayer._unitsInfluence[0] * _deffender.Count -
+                                _attacker._unit.Attack * _mageInfluence * _attackCount *
+                                _attacker._unit.StrongCoefitient) / (_deffender._unit.Health *
+                                                                     _deffenderPlayer._unitsInfluence[0]));
                     yield break;
                 }
 
                 if (_deffender._unit is Assasin)
                 {
                     yield return _deffender.Count =
-                        (int) Math.Floor(
-                            (_deffender._unit.Health * _deffenderPlayer._unitsInfluence[1] * _deffender.Count -
-                             _attacker._unit.Attack * _mageInfluence * _attackCount * _attacker._unit.WeakCoeficient) /
-                            _deffender._unit.Health * _deffenderPlayer._unitsInfluence[1]);
+                        (int) ((_deffender._unit.Health * _deffenderPlayer._unitsInfluence[1] * _deffender.Count -
+                                _attacker._unit.Attack * _mageInfluence * _attackCount * _attacker._unit.WeakCoeficient) /
+                               (_deffender._unit.Health * _deffenderPlayer._unitsInfluence[1]));
                     yield break;
                 }
 
                 if (_deffender._unit is Mage)
-                    yield return _deffender.Count = (int) Math.Floor(
-                        (_deffender._unit.Health * _deffenderPlayer._unitsInfluence[2] * _deffender.Count -
-                         _attacker._unit.Attack * _mageInfluence * _attackCount) /
-                        _deffender._unit.Health * _deffenderPlayer._unitsInfluence[2]);
+                    yield return _deffender.Count = (int) ((_deffender._unit.Health * _deffenderPlayer._unitsInfluence[2] * _deffender.Count -
+                                                            _attacker._unit.Attack * _mageInfluence * _attackCount) /
+                                                           (_deffender._unit.Health * _deffenderPlayer._unitsInfluence[2]));
             }
         }
     }

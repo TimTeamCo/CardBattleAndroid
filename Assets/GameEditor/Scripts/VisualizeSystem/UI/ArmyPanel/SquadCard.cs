0c454812b951fdb8996f5853cb6b97b5a5dd26a5
Ilya <ponomarenko.illia@kfccte-nau.ukr.education>
2022-11-11 22:12:21 +0100

    Added new Start Scene

---
+++ Assets/GameEditor/Scripts/VisualizeSystem/UI/ArmyPanel/SquadCard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TTBattle.UI
+{
+    public class SquadCard : MonoBehaviour
+    {
+        [SerializeField] public Text UnitsNumber;
+        [SerializeField] public Text HPCellAtrtibute;
+        [SerializeField] public Text APCellAtrtibute;
+
+        public void SetHPCellAtrtibute(float unitInfluence, int unitHP)
+        {
+            int hpCellAttribute = (int)((unitHP*unitInfluence)-unitHP);
+            
+            if (hpCellAttribute > 0)
+            {
+                HPCellAtrtibute.text = $"+ {hpCellAttribute}";
+            }
+            
+            if (hpCellAttribute == 0)
+            {
+                HPCellAtrtibute.text = "0";
+            }
+
+            if (hpCellAttribute < 0)
+            {
+                HPCellAtrtibute.text = $"- {hpCellAttribute}";
+            }
+        }
+        
+        public void SetAPCellAtrtibute(float unitInfluence, int unitAP)
+        {
+            int apCellAttribute = (int)((unitAP*unitInfluence)-unitAP);
+            
+            if (apCellAttribute > 0)
+            {
+                HPCellAtrtibute.text = $"+ {apCellAttribute}";
+            }
+            
+            if (apCellAttribute == 0)
+            {
+                HPCellAtrtibute.text = "0";
+            }
+
+            if (apCellAttribute < 0)
+            {
+                HPCellAtrtibute.text = $"- {apCellAttribute}";
+            }
+        }
+    }
+}

b2088fdd3a0eaa3b1ec965547f54f0d110894f5d
Timur Perepelitsa <devsspacegame@gmail.com>
2022-11-19 14:33:07 +0200

    Add MapZone1

--- Assets/Scripts/Map/MapZone.cs
+++ Assets/Scripts/Map/MapZone.cs
@@ -1,10 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Army;
+using UnityEngine;
 
 namespace Map
 {
-    [CreateAssetMenu(fileName = "MapZone", menuName = "ScriptableObject/Map/MapZone", order = 0)]
+    [CreateAssetMenu(fileName = "MapZone0", menuName = "ScriptableObject/Map/MapZone", order = 0)]
     public class MapZone : ScriptableObject
     {
         public BiomType biom;
+        public int zoneID ;
+        public List<UnitType> UnitTypes;
+        public List<int> buffValue;
+        public Dictionary<UnitType, int> BuffZone;
+
+        private void OnValidate()
+        {
+            if (UnitTypes.Count != buffValue.Count)
+            {
+                return;
+            }
+            
+            BuffZone = new Dictionary<UnitType, int>();
+            for (int i = 0; i < UnitTypes.Count; i++)
+            {
+                BuffZone.Add(UnitTypes[i], buffValue[i]);
+            }
+            Debug.Log($"New Dic {BuffZone.Keys.Count} {BuffZone.Values.Count}");
+        }
     }
 }

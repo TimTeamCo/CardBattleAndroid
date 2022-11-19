660a273bbeb5be6fc25c10e80e7269501816f602
Timur Perepelitsa <devsspacegame@gmail.com>
2022-11-19 21:46:45 +0200

    Card and Item card

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Card
+{
+    [CreateAssetMenu(fileName = "Card", menuName = "ScriptableObject/Cards/Card", order = 0)]
+    public class Card : ScriptableObject
+    {
+        public string name;
+        public Sprite frame;
+        public Sprite art;
+    }
+}
---
+++ Assets/Scripts/Card/ItemCard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Card
+{
+    [CreateAssetMenu(fileName = "ItemCard", menuName = "ScriptableObject/Cards/ItemCard", order = 1)]
+    public class ItemCard : Card
+    {
+        
+    }
+}

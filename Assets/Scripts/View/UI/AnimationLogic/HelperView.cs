1cdf1d2e1bf6cc0845e557cf57d60dcec4576888
Timur Perepelitsa <devsspacegame@gmail.com>
2023-05-07 19:32:46 +0300

    clear trash

--- Assets/Scripts/View/UI/AnimationLogic/HelperView.cs
+++ Assets/Scripts/View/UI/AnimationLogic/HelperView.cs
@@ -9,7 +9,6 @@ public class HelperView : MonoBehaviour
     [SerializeField] private Image _helperEmotion;
     [SerializeField] private HelperEmotionSprites _helperEmotions;
     [SerializeField] private float _animationDuration = 3.0f;
-    private Tween _tween;
 
     private void Start()
     {
